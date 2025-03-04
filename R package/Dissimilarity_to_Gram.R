0a3c00f8cf3801120f952ced9e1a23e509fa5649
Wenhao Li <liwh0904@gmail.com>
2025-03-04 00:22:53 -0800

    Delete R package directory

--- R package/Dissimilarity_to_Gram.R
+++
@@ -1,7 +0,0 @@
-Dissimilarity_to_Gram <- function(D){
-  # convert dissimilarity matrix to Gram matrix
-  R = ncol(D)
-  J = diag(R) - (matrix(1,R,1) %*% matrix(1,1,R))/R
-  Gram = -0.5 * J %*% D^2 %*% J
-  return(Gram)
-}
