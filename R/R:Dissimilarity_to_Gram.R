db45cc1b860d5d4b494a00c5195d0f00c1d76af6
Wenhao Li <liwh0904@gmail.com>
2025-03-18 16:34:08 -0700

    Update and rename Dissimilarity_to_Gram.R to R:Dissimilarity_to_Gram.R

--- R/R:Dissimilarity_to_Gram.R
+++ R/R:Dissimilarity_to_Gram.R
@@ -1,3 +1,10 @@
+#' Convert Dissimilarity Matrix to Gram Matrix
+#'
+#' This function converts a dissimilarity matrix to a Gram matrix.
+#'
+#' @param D A dissimilarity matrix.
+#' @return A Gram matrix.
+#' @export
 Dissimilarity_to_Gram <- function(D){
   # convert dissimilarity matrix to Gram matrix
   R = ncol(D)
