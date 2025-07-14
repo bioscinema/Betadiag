d817caec3e53b328d1df04121bb3885e3ef4af87
ZihanZhuCase <zxz1586@case.edu>
2025-07-14 15:59:12 -0400

    Update euclidean_check.R

--- R/euclidean_check.R
+++ R/euclidean_check.R
@@ -18,10 +18,9 @@
 euclidean_check <- function(G) {
   eig <- eigen(G)
   lambda <- eig$values
-  lambda[abs(lambda) < 1e-12] <- 0
   neg_sum <- sum(abs(lambda[lambda < 0]))
   total_sum <- sum(abs(lambda))
   ratio <- neg_sum / total_sum
   
-  return(list(FNI = ratio, is.Euclidean = ifelse(ratio > 0, 0, 1)))
+  return(list(FNI = ratio, is.Euclidean = ifelse(ratio > 1e-12, 0, 1)))
 }
